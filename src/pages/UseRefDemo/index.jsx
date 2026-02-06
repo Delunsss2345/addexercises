aeb1cc16d5ddf2d97344611bd9136140444f2cd5
Phạm Thanh Huy <huyp7922@gmail.com>
2026-02-06 08:29:17 +0700

    feat(useRef) : xong useRef và minh chứng

--- src/pages/UseRefDemo/index.jsx
+++ src/pages/UseRefDemo/index.jsx
@@ -1,5 +1,18 @@
-const UseRefDemo = () => {
-    return <></>
-}
+import { useRef, useState } from "react";
 
-export default UseRefDemo;
+export default function UseRefDemo() {
+    const renderCountRef = useRef(0);
+    const [value, setValue] = useState(0);
+
+    renderCountRef.current += 1;
+
+    return (
+        <div>
+            <p>Số lần trang này render: {renderCountRef.current}</p>
+
+            <button onClick={() => setValue((v) => v + 1)}>
+                Click (value: {value})
+            </button>
+        </div>
+    );
+}
